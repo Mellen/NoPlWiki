1e4cf227b0156b9a48cdda34282d6adcda8b384b
Matt Ellen <matt@matthewellen.co.uk>
2012-10-07 17:37:05 +0100

    fixed bug caused because data should be called page_data refactored createBook into createPage built the contents got POST to update the page

--- classes/book.py
+++ classes/book.py
@@ -11,8 +11,8 @@ class book(object):
     exposed = True
 
     def GET(self, title=None, page_name='main_page'):
-        self.collection = self.db[title]
-        page_data = self.collection.find_one('{\'page_name\':\''+page_name+'\', \'title\':\''+title+'\'}')
+        collection = self.db[title]
+        page_data = collection.find_one({'page_name':page_name})
         if page_name == 'main_page':
             template = Template(filename='templates/book.html')
         else:
@@ -20,25 +20,41 @@ class book(object):
 
         if page_data is None:
             if page_name == 'main_page':
-                data = self.createBook(title)
-                data['contents'] = ''
-                data['page_body'] = ''
+                page_data = self.createPage(collection)
+                page_data['contents'] = ''
             else:
-                data = self.createPage(title, page_name)
-                data['page_body'] = ''
+                page_data = self.createPage(collection, page_name)
+            page_data['page_body'] = ''
         else:
             if page_name == 'main_page':
-                data['contents'] = ''
-            data['page_body'] = markdown.markdown(data['page_body_raw'])
-        return template.render(**data)
+                page_data['contents'] = self.buildContents(collection, title)
+            page_data['page_body'] = markdown.markdown(page_data['page_body_raw'])
 
-    def createBook(self, title):
-        data = {'title': title
-               ,'page_name': 'main_page'
+        page_data['title'] = title
+        page_data['page_name'] = page_name
+
+        return template.render(**page_data)
+
+    def createPage(self, collection, page_name='main_page'):
+        data = {'page_name': page_name
                ,'page_body_raw': ''}
-        self.collection.insert(data)
+        collection.insert(data)
         return data
 
+    def buildContents(self, collection, title):
+        contents = collection.find({}, {'_id':0, 'page_name':1})
+        contents_md = ''
+        extra = ''
+        if 'title' in cherrypy.request.params:
+            extra = 'book/'+title+'/'
+        for item in contents:
+            if item['page_name'] != 'main_page':
+                contents_md += ' - [{0}]({1}{0})\n'.format(item['page_name'], extra)
+        return markdown.markdown(contents_md)
+
     def POST(self, title=None, page_name='main_page', page_body_text=None):
-        self.collection = self.db[title]
-        return self.GET(title)
+        collection = self.db[title]
+        where = {'page_name':page_name}
+        what = {'page_name':page_name, 'page_body_raw':page_body_text}
+        collection.update(where, what, False, False)
+        return self.GET(title, page_name)
